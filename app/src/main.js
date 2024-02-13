bf5b4f1a011fc559c63d959ad8029f7457707771
cyph404 <nicoleblanchette05@gmail.com>
2024-02-13 18:28:06 -0500

    test

--- app/src/main.js
+++ app/src/main.js
@@ -1,11 +1,13 @@
-const arrow = document.querySelector('.arrow');
-const speed = document.querySelector('.speed-value');
+const arrow = document.querySelector(".arrow")
+const speed = document.querySelector(".speed-value")
+const coords = document.querySelector("#coords")
+
 
 navigator.geolocation.watchPosition((data) => {
   console.log(data);
   speed.textContent = data.coords.speed;
-  arrow.style.transform = `rotate(${data.coords.heading}deg)`
+  arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  coords.textContent = data.coords.latitude
 }, (err) => {
-  console.error(err)
-  alert("silly! this compass won't work without your location info!")
+  console.error(err);
 });
