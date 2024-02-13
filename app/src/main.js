125d8286ebbfe49300e6d5134a55858fe1933ca9
cyph404 <nicoleblanchette05@gmail.com>
2024-02-13 18:50:21 -0500

    can someone help me with lan testing

--- app/src/main.js
+++ app/src/main.js
@@ -2,12 +2,22 @@ const arrow = document.querySelector(".arrow")
 const speed = document.querySelector(".speed-value")
 const coords = document.querySelector("#coords")
 
+// navigator.geolocation.watchPosition(
+// 	data => {
+// 		console.log(data)
+// 		console.log(data.coords.heading)
+// 		speed.textContent = data.coords.speed
+// 		arrow.style.transform = `rotate(${data.coords.latitude}deg)`
+// 		coords.textContent = data.coords.latitude
+// 	},
+// 	err => {
+// 		console.error(err)
+// 	}
+// )
 
-navigator.geolocation.watchPosition((data) => {
-  console.log(data);
-  speed.textContent = data.coords.speed;
-  arrow.style.transform = `rotate(${data.coords.heading}deg)`;
-  coords.textContent = data.coords.latitude
-}, (err) => {
-  console.error(err);
-});
+const handleOrientation = e => {
+  const compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
+  arrow.style.transform = `rotate(${compass}deg)`
+}
+
+window.addEventListener("deviceorientation", handleOrientation)
